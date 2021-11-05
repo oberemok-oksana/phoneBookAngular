ff4c45dbbb4d967d77b7ac76bf4ac8ab51dfdc9c
Oksana Oberemok <Pylypenkok@gmail.com>
2021-11-05 13:49:24 +0200

    Renaming

--- src/app/components/app.component.ts
+++ src/app/components/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { User } from '../models/user';
-import { FormsModule } from '@angular/forms';
 import { Login } from '../models/login';
 import { Contact } from '../models/contact';
 
@@ -14,46 +13,55 @@ export class AppComponent {
   public login: Login = new Login('', '');
   public users: User[] = [new User('Oksana', '12345', '123345')];
   public currentUser?: User;
-  public contact: Contact = new Contact('', '', '');
-  public contacts: Contact[] = [
-    new Contact('Boba', 'phone', '09956874585'),
-    new Contact('Puma', 'email', 'puma@gmail.com'),
-  ];
+  public contact: Contact = new Contact('', 'phone', '');
+  public contacts: Contact[] = [];
+  public filteredContacts: Contact[] = this.contacts;
   public types: string[] = ['phone', 'email'];
   public activeContact: Contact | null = null;
   public showModal: boolean = false;
-  public message: string = 'Please, fill out all the fields.';
-  // public search: Contact[] = [];
-  // public findTypes: string[] = ['name', 'phone/email'];
-  // public foundContact: Contact | null = null;
+  public message?: string;
+  public emptyField: boolean = false;
+  public searchText: string = '';
+  public searchType: 'find-by-name' | 'find-by-value' = 'find-by-name';
 
-  addUser() {
+  signup() {
     if (
       this.user.login === '' ||
       this.user.password === '' ||
       this.user.birthdate === ''
     ) {
-      this.showModal = true;
-      console.log(111);
+      this.showModalMessage('Please, fill out all the fields');
     } else {
       this.users.push(this.user);
       this.user = new User('', '', '');
-      console.log(this.users);
     }
   }
 
-  findUser(login: string, password: string) {
-    this.currentUser = this.users.find((u) => {
-      return u.login === login && u.password === password;
-    });
-
-    console.log(this.currentUser);
+  signin(login: string, password: string) {
+    if (this.login.login === '' || this.login.password === '') {
+      this.showModalMessage('Please, fill out all the fields');
+    } else {
+      this.currentUser = this.users.find((u) => {
+        return u.login === login && u.password === password;
+      });
+      if (!this.currentUser) {
+        this.showModalMessage('Wrong login or password');
+      }
+    }
   }
 
   addContact(name: string, type: string, value: string) {
-    this.contacts.push(new Contact(name, type, value));
-    this.contact = new Contact('', '', '');
-    console.log(this.contacts);
+    if (
+      this.contact.name === '' ||
+      this.contact.type === '' ||
+      this.contact.value === ''
+    ) {
+      this.showModalMessage('Please, fill out all the fields');
+    } else {
+      this.contacts.push(new Contact(name, type, value));
+      this.contact = new Contact('', 'phone', '');
+      this.showFilteredContacts();
+    }
   }
 
   setToActive(contact: Contact) {
@@ -65,21 +73,37 @@ export class AppComponent {
     if (this.activeContact === contact) {
       this.activeContact = null;
     }
+    this.showFilteredContacts();
   }
 
-  // findContact(type: string, value: string) {
-  //   this.search = this.contacts.filter(
-  //     (c) => c.type === type && c.value === value
-  //   );
-  //   console.log(this.search);
-  // }
+  showFilteredContacts() {
+    if (this.searchType === 'find-by-name') {
+      this.filteredContacts = this.contacts.filter((c) =>
+        c.name
+          .toLocaleLowerCase()
+          .startsWith(this.searchText.toLocaleLowerCase())
+      );
+    } else {
+      this.filteredContacts = this.contacts.filter((c) =>
+        c.value
+          .toLocaleLowerCase()
+          .startsWith(this.searchText.toLocaleLowerCase())
+      );
+    }
+  }
 
-  resetChosenContact() {
+  resetFindContact() {
     this.activeContact = null;
+    this.filteredContacts = this.contacts;
   }
 
-  exit() {
+  logout() {
     this.currentUser = undefined;
     this.login = new Login('', '');
+  }
+
+  showModalMessage(message: string) {
+    this.message = message;
+    this.showModal = true;
   }
 }
